c4aecf3244c49878b9e9cdfabcda9bf575f46d79
Jony09-thx <jonyperezmata@gmail.com>
2025-03-29 03:51:42 -0600

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -10,7 +10,7 @@ try{
     $conn = new PDO("pgsql:host=$host;dbname=$dbname; port=$port", $user, $password);
     //echo "Conexion exitosa";
 }catch(PDOException $exp){
-    echo "Error en la conexion";
+    echo "Error en la conexion" . $exp->getMessage();
 }
 
 ?>
