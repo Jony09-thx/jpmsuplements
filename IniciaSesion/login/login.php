24b9cefbf74e69a4d2e2f48cc1e79a06bad15e01
Jony09-thx <jonyperezmata@gmail.com>
2025-03-29 03:48:07 -0600

    Update login.php

--- IniciaSesion/login/login.php
+++ IniciaSesion/login/login.php
@@ -2,7 +2,7 @@
 session_start();
 
 try {
-    $pdo = new PDO("pgsql:host=dpg-cvjr6uk9c44c73cbt25g-a.oregon-postgres.render.com;dbname=jpmsuple2", "jpmsuple2", "95tOJxSX9QTIuxnyg7dkBSeFTVvj9xGO");
+    $pdo = new PDO("pgsql:host=dpg-cvjr6uk9c44c73cbt25g-a.oregon-postgres.render.com;port=5432;dbname=jpmsuple2", "jpmsuple2", "95tOJxSX9QTIuxnyg7dkBSeFTVvj9xGO");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "SELECT u.id_usuario, u.usuario, u.password, u.id_persona, u.rol
