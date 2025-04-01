1b6bd30751b7cf059e9d7b5b53cfe1658e582fd0
Jony09-thx <jonyperezmata@gmail.com>
2025-04-01 09:47:40 -0600

    Update login.php

--- IniciaSesion/login/login.php
+++ IniciaSesion/login/login.php
@@ -21,7 +21,7 @@ try {
         if ($usuario['rol'] == 'administrador') {
             header("Location: ../../CRUD/listar.php");
             exit();
-        } else if ($usuario['rol'] == 'cliente') {
+        } else if ($usuario['rol'] == '[default]') {
             header("Location: https://jpmsuplements.onrender.com/index.php");
             exit();
         }
